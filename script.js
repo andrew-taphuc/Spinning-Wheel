8f17200c87973d7c2223cf6557de94d7ddccb278
Ta Hong Phuc <taphuc1@gmail.com>
2024-04-20 00:42:52 +0700

    Still not working

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ const spinBtn = document.getElementById("spin-btn");
 const finalValue = document.getElementById("final-value");
 //object that store values of min and max angle for a value
 const rotationValues = [
-    { minDegree: 0, maxDegree: 30, value: 'ăn dookii'},
-    { minDegree: 31, maxDegree: 90, value: 'ăn kem tràng tiền'},
-    { minDegree: 91, maxDegree: 150, value: 'đi ngủ'},
-    { minDegree: 151, maxDegree: 210, value: 'đi study Date'},
-    { minDegree: 211, maxDegree: 270, value: 'đi hồ Tây'},
-    { minDegree: 271, maxDegree: 330, value: 'chơi bowling'},
-    { minDegree: 331, maxDegree: 360, value: 'ăn dookii'},
-
+    { minDegree: 0,     maxDegree: 15,  value: 'đi studydate'},
+    { minDegree: 16,    maxDegree: 45,  value: 'đi ngủ'},
+    { minDegree: 46,    maxDegree: 75,  value: 'đi chơi bowling'},
+    { minDegree: 76,    maxDegree: 105, value: 'đi thủy cung'},
+    { minDegree: 106,   maxDegree: 135, value: 'đi amusement park'},
+    { minDegree: 136,   maxDegree: 165, value: 'ăn Hey Pelo Taco'},
+    { minDegree: 166,   maxDegree: 195, value: 'đi date ở AEON Mall'},
+    { minDegree: 196,   maxDegree: 225, value: 'đi ăn Bingsu'},
+    { minDegree: 226,   maxDegree: 255, value: 'xem phim'},
+    { minDegree: 256,   maxDegree: 285, value: 'đi hồ Gươm ăn kem Tràng Tiền'},
+    { minDegree: 286,   maxDegree: 315, value: 'chơi đất nặn'},
+    { minDegree: 316,   maxDegree: 345, value: 'đi lượn hồ Tây'},
+    { minDegree: 346,   maxDegree: 460,  value: 'đi studydate'},
 ];
 //size of each piece
-const data = [16, 16, 16, 16, 16, 16];
+const data = [16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16];
 
 //background color for each piece
 var pieColors = [
@@ -22,6 +27,12 @@ var pieColors = [
     "#8b35bc", 
     "#b163da",
     "#8b35bc", 
+    "#b163da",
+    "#8b35bc", 
+    "#b163da",
+    "#8b35bc",
+    "#b163da",
+    "#8b35bc",  
     "#b163da"];
 
 //Create chart
@@ -32,7 +43,8 @@ let myChart = new Chart(wheel, {
     type: "pie",
     data: {
       //Labels(values which are to be displayed on chart)
-      labels: ['🍦', "🥘", '🎳', '⛵️', '📚 ', '🛌'],
+      labels: [ '🍿','🎳','🛌', '📚', '🚤','🏺', '🍦','🍧', '🍣', '🌮', '🎯', '🦦'],
+
       //Settings for dataset/pie
       datasets: [
         {
@@ -67,23 +79,14 @@ const valueGenerator = (angleValue) => {
     for (let i of rotationValues) {
       //if the angleValue is between min and max then display it
       if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-        finalValue.innerHTML = `<p>Em đã chọn ${i.value} cùng Andrew</p>`;
+        finalValue.innerHTML = `<p>Em chọn ${i.value} cùng Andrew</p>`;
         spinBtn.disabled = false;
         break;
       }
     }
   };
 
-//   //Spinner Count
-// let count = 0;
-//   //100 rotation of random and end at result
-// let resultValue = 101;
-// //start spinning
-// spinBtn.addEventListener("click"), () => {
-//     spinBtn.diabled = true;
-// // empty final value
-// finalValue.innerHTML = `<p>Good Luck!</p>`
-// //generate random degree
+
 
 //Spinner count
 let count = 0;
@@ -97,11 +100,12 @@ spinBtn.addEventListener("click", () => {
   //Generate random degrees to stop at
   let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
   //Interval for rotation animation
-  let rotationInterval = window.setInterval(() => {
-    //Set rotation for piechart
-    /*
-    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
-    */
+  let rotationInterval = window.setInterval(() => { //Set rotation for piechart
+    
+    /* Initially to make the piechart rotate faster we set resultValue to 101 so it rotates
+    101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation
+    we rotate by 1 degree at a time.*/
+
     myChart.options.rotation = myChart.options.rotation + resultValue;
     //Update chart with new value;
     myChart.update();
